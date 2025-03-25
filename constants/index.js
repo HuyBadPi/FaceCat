8a96cca717c1fd326c1beff9ec0bc0fa5fb14aae
HuyBadPi <tranhuylqd@gmail.com>
2025-03-26 00:28:16 +0100

    Authentication Function

---
+++ constants/index.js
@@ -0,0 +1,2 @@
+export const supabaseUrl = 'https://lbkluahlpbeajlpwkgsm.supabase.co';
+export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxia2x1YWhscGJlYWpscHdrZ3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NTI2OTYsImV4cCI6MjA1ODQyODY5Nn0.gBPwgEO4KLOUzLLBCSKie7Bu0Gt4IRBjkOM0bTTUrXg';
